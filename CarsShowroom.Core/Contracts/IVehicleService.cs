fb5d887e59a2685a420c7f9c6fed062da8493398
konfucianeca <vladogg56@gmail.com>
2025-01-29 15:38:44 +0200

    Infrastructure lyier, view model templates added

---
+++ CarsShowroom.Core/Contracts/IVehicleService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarsShowroom.Core.Contracts
+{
+    public interface IVehicleService
+    {
+        Task<IEnumerable<VehicleIndexServiceModel>> LastThreeVehiclesAsync();
+    }
+}
---
+++ CarsShowroom.Core/Models/Home/VehicleIndexServiceModel.cs
@@ -0,0 +1,12 @@
+﻿namespace CarsShowroom.Core.Models.Home
+{
+    public class VehicleIndexServiceModel
+    {
+        public int Id { get; set; }
+        public string ImageUrl { get; set; } = string.Empty;
+        public string Model { get; set; }= string.Empty;
+        public string YearOfProduction { get; set; } = string.Empty;
+        public string EngineType { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+}
