cac2b4cf63f9237bcf237463a855468a8fa0989c
konfucianeca <vladogg56@gmail.com>
2025-02-11 15:46:53 +0200

    Created VehiclesDetailsViewModel

--- CarsShowroom.Core/Models/Vehicle/VehicleDetailsViewModel.cs
+++ CarsShowroom.Core/Models/Vehicle/VehicleDetailsViewModel.cs
@@ -1,22 +1,15 @@
-﻿using CarsShowroom.Infrastructure.Data.Models.Enums;
+﻿using CarsShowroom.Core.Models.Home;
 
 namespace CarsShowroom.Core.Models.Vehicle
 {
-    public class VehicleDetailsViewModel
+    public class VehicleDetailsViewModel : VehicleIndexServiceModel
     {
-        public int Id { get; set; }
-        public string Model { get; set; } = string.Empty;
-        public Condition? Condition { get; set; }
-        public string YearOfProduction { get; set; } = null!;
         public string Region { get; set; } = string.Empty;
-        public Gearbox Gearbox { get; set; }
+        public string Gearbox { get; set; } = string.Empty;
         public string Color { get; set; } = string.Empty;
         public int Mileage { get; set; }
         public string Features { get; set; } = string.Empty;
-        public decimal Price { get; set; }
-        public string ImageUrl { get; set; } = string.Empty;
         public int ManufacturerId { get; set; }
-        public Engine EngineType { get; set; }
         public int Displacement { get; set; }
         public int Power { get; set; }
     }
