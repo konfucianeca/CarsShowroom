ac2a78ed8347e1970c49616b693aec277b88262a
konfucianeca <vladogg56@gmail.com>
2025-02-20 14:19:44 +0200

    Database Enumerations amended

--- CarsShowroom.Infrastructure/Migrations/20250220094006_EnumsAmended.Designer.cs
+++ CarsShowroom.Infrastructure/Migrations/20250220094006_EnumsAmended.Designer.cs
@@ -4,6 +4,7 @@ using CarsShowroom.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CarsShowroom.Infrastructure.Migrations
 {
     [DbContext(typeof(CarsShowroomDbContext))]
-    partial class CarsShowroomDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250220094006_EnumsAmended")]
+    partial class EnumsAmended
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CarsShowroom.Infrastructure/Migrations/20250220094006_EnumsAmended.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarsShowroom.Infrastructure.Migrations
+{
+    public partial class EnumsAmended : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Condition",
+                table: "Vehicles",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "302ded00ee6f4fea7f65fd9f66001b7f62f7673a02459f6930ba396bc26412de",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "b8e9d2c1-0108-43dd-a8cf-ec678ee13d35", "AQAAAAEAACcQAAAAECZ8MnLTJ1isDc10OJmKoJN5/oidEzwrQPefdx1xzPVX7aVujE78I6AKXQEz2tnZIA==", "3b611b5d-3e85-4e85-b0e0-7f43de8fe5c1" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Condition",
+                table: "Vehicles",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "302ded00ee6f4fea7f65fd9f66001b7f62f7673a02459f6930ba396bc26412de",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "c4008d68-4762-40de-9961-a29aff7ecc52", "AQAAAAEAACcQAAAAENDhPlyejNc1uJUEM8g2DtecV1ibKVVo8o5zKWPseUsKSeCRqIbkFV+Ud7W9YXQx3w==", "b255ebac-d256-4ed0-9245-a60e3a8fd5d8" });
+        }
+    }
+}
